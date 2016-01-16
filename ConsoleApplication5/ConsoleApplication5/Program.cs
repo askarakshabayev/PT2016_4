73dcf9cdc437d4cc64f4f30e74beef24d2fefa1a
askarakshabayev <askar.akshabayev@gmail.com>
2016-01-16 11:28:45 +0600

    added lab1 branch

--- ConsoleApplication5/ConsoleApplication5/Program.cs
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -10,19 +10,6 @@ namespace ConsoleApplication5
     {
         static void Main(string[] args)
         {
-            // int, long, byte
-            // double, float
-            // char 'a'-> 97, 'A' -> 65, '0' - 48, ' ' - 32
-            // string
-            //string s = "hello worldllo";
-            //int l = s.Length; // length of the string
-            //string s1 = s.Substring(2, 3); // 2 - start index, 3 - length of substring
-            //string s2 = s.Replace("llo", "qwerty"); // heqwerty world
-            //int position = s.IndexOf("llo", 3); // position = 2
-
-
-
-
             Console.ReadKey();
         }
     }
