3845e9cc0acfb537f9f90e7446f5ccc63875310d
askarakshabayev <askar.akshabayev@gmail.com>
2016-01-16 11:14:22 +0600

    added example of reading two numbers from one line

--- ConsoleApplication5/ConsoleApplication5/Program.cs
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -11,17 +11,10 @@ namespace ConsoleApplication5
         static void Main(string[] args)
         {
             // 4 5
-            // string.Split()
-
-            string s = "hello world 1 2 3";
-            string[] token = s.Split();
-            // token[0] = hello
-            // token[1] = world
-
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine(token[i]);
-            }
+            string[] token = Console.ReadLine().Split();
+            int a = int.Parse(token[0]);
+            int b = int.Parse(token[1]);
+            Console.WriteLine(a + b);
             Console.ReadKey();
         }
     }
